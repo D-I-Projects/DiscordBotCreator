eb5f9a04fad91721076a91dc16483c1046503456
wfxey <158351052+wfxey@users.noreply.github.com>
2024-06-22 13:13:22 +0200

    Add files via upload
    Signed-off-by: wfxey <158351052+wfxey@users.noreply.github.com>

--- discord_bot_functions.py
+++ discord_bot_functions.py
@@ -1,88 +1,84 @@
-"""
-    Normal Use:
-    import discord_bot_fujnctions as dbf
-    dbf.start_bot("Bot TOKEN", [["command_type", "command name", "Response-Message (With variables)", "command-description"], ["Way more commands"]])
-"""
+#def start_bot():
+#    TOKEN = "YOUR_DISCORD_BOT_TOKEN"
+#    adding_command_list = [
+#        ["response_command", "hi", "Hi!", "Say you hi"],
+#        ["response_command", "time", "Current date is {dateVar}, {timeVar}", "Say the current date and time"]
+#    ]
+#    dbf.start_bot(TOKEN, adding_command_list)
 
 from discord.ext import commands
 from discord import app_commands, Intents
 import discord
-from datetime import datetime, date
-import logging
+import datetime
 import os
+import logging
 
+def log_settings():
+    log_dir = os.path.join("Logs")
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    current_datetime = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    log_filename = os.path.join(log_dir, f"Destor_{current_datetime}.log")
+
+    logger = logging.getLogger("Destor")
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    file_handler = logging.FileHandler(log_filename)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return logger
+
+logger = log_settings()
 
 class Client(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix='.', intents=Intents().all())
 
     async def on_ready(self):
-        print("Logged in as " + self.user.name)
+        logger.info("Logged in as " + self.user.name)
         synced = await self.tree.sync()
-        if len(synced) == 1:
-            print("Slash CMDs Synced " + str(len(synced)) + " Command")
-        else:
-            print("Slash CMDs Synced " + str(len(synced)) + " Commands")
-
+        logger.info(f"Slash CMDs Synced {len(synced)} Command{'s' if len(synced) != 1 else ''}")
 
 class ResponseCommand:
     def __init__(self, command_name, response_text, client, description="..."):
         @client.tree.command(name=command_name, description=description)
-        async def command(interaction: discord.Interaction):
+        async def my_command(interaction: discord.Interaction):
             await interaction.response.send_message(response_text)
-
-class ArgumentCommand:
-    def __init__(self, command_name, response_text, client, description="..."):
-        @app_commands.command(name=command_name, description=description)
-        async def command(interaction: discord.Interaction, file_name: str, content: str):
-            await interaction.response.send_message(response_text)
-
-
-        client.tree.add_command(command)
-
-
 
 class BotVariables:
-    def __init__(self):
-        pass
+    @staticmethod
+    def time_var():
+        return datetime.datetime.now().strftime("%H:%M")
 
-    def timeVar(self):
-        return datetime.now().strftime("%H:%M")
-
-    def dateVar(self):
-        return date.today()
-
+    @staticmethod
+    def date_var():
+        return datetime.date.today().strftime("%Y-%m-%d")
 
 def start_client(token, adding_command_list):
     client = Client()
     add_command(adding_command_list, client)
     client.run(token)
 
-def replace_BotVars(command):
-    replace_rules = [["{timeVar}", str(BotVars.timeVar())], ["{dateVar}", str(BotVars.dateVar())]]
-    for rules in replace_rules:
-        command = command.replace(rules[0], rules[1])
+def replace_bot_vars(command):
+    replace_rules = [["{timeVar}", BotVariables.time_var()], ["{dateVar}", BotVariables.date_var()]]
+    for old, new in replace_rules:
+        command = command.replace(old, new)
     return command
-
 
 def add_command(command_list, client):
     for command in command_list:
-        if command_list[0] == "response_command":
-            command_name, response_text, description = command[1], replace_BotVars(command[2]), command[3]
-            ResponseCommand(command_name, response_text, client, description)
-        elif command_list[0] == "argument_command":
-            command_name, response_text, description, = command[1], replace_BotVars(command[2]), command[3]
-            ArgumentCommand(command_name, response_text, client, description)
-            
-
+        command_name, response_text, description = command[1], replace_bot_vars(command[2]), command[3]
+        ResponseCommand(command_name, response_text, client, description)
 
 def start_bot(TOKEN, adding_command_list):
-    start_client(TOKEN, adding_command_list)
-
-
-BotVars = BotVariables()
-
-if __name__ == "__main__":
-    TOKEN = ""
-    adding_command_list = [["argument_command", "hi", "Hi!", "Say you hi"]]
     start_client(TOKEN, adding_command_list)
